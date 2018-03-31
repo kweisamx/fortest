e029dfe8a2771d9834d000f958f85ce1f1f8839f
kweisamx <kweisamx0322@gmail.com>
2018-03-31 21:13:50 +0800

    fix the typo

--- exprand.go
+++ exprand.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 	"math/rand"
-    "os/exec"
-    "strconv"
-    "time"
+	"os/exec"
+	"strconv"
+	"time"
 )
 
 func main() {
 	for {
-		time_curl := rand.ExpFloat64() / 1
-        time_curl = time_curl * 10
-        time.Sleep(time.Duration(int(time_curl)) * time.Second)
-        sp := strconv.Itoa(int(time_curl))
-        fmt.Printf("%s s, send curl to localhost:30001/stress\n", sp)
-        cmd := exec.Command("bash", "curl2.sh", sp)
-        cmd.Run()
+		time_curl := rand.ExpFloat64() / 5
+		time_curl = time_curl * 10
+		time.Sleep(time.Duration(int(time_curl)) * time.Second)
+		sp := strconv.Itoa(int(time_curl))
+		fmt.Printf("%s s, send curl to localhost:30001/stress\n", sp)
+		cmd := exec.Command("bash", "curl2.sh", sp)
+		cmd.Run()
 	}
 }
--- node_exporter/src/store/store.go
+++ node_exporter/src/store/store.go
@@ -23,7 +23,7 @@ func CsvInit(RefreshTime int) {
 	f.WriteString("\xEF\xBB\xBF")
 
 	w := csv.NewWriter(f)
-	w.Write([]string{"nowtime", "pod", "m1_cpu", "m2_cpu", "m3_cpu", "m1_mem", "m2_mem", "m3_mem", "refreshtime"})
+	w.Write([]string{"nowtime", "m1_cpu", "m2_cpu", "m3_cpu", "m1_mem", "m2_mem", "m3_mem", "pod", "refreshtime"})
 	w.Flush()
 }
 func CsvStore(nowtime int, pod int, m1_cpu float64, m2_cpu float64, m3_cpu float64, m1_mem float64, m2_mem float64, m3_mem float64, RefreshTime int) {
@@ -38,9 +38,9 @@ func CsvStore(nowtime int, pod int, m1_cpu float64, m2_cpu float64, m3_cpu float
 	w := csv.NewWriter(f)
 	s := make([]string, 9)
 	s[0] = strconv.Itoa(nowtime)
-	s[1] = strconv.Itoa(pod)
-	s[2], s[3], s[4] = FloatToString(m1_cpu), FloatToString(m2_cpu), FloatToString(m3_cpu)
-	s[5], s[6], s[7] = FloatToString(m1_mem), FloatToString(m2_mem), FloatToString(m3_mem)
+	s[1], s[2], s[3] = FloatToString(m1_cpu), FloatToString(m2_cpu), FloatToString(m3_cpu)
+	s[4], s[5], s[6] = FloatToString(m1_mem), FloatToString(m2_mem), FloatToString(m3_mem)
+	s[7] = strconv.Itoa(pod)
 	s[8] = strconv.Itoa(RefreshTime)
 	fmt.Println(s)
 	w.Write(s)
