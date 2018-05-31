4ba7fa37b775f167e35c5c9e39c4174f497a9f95
kweisamx <kweisamx0322@gmail.com>
2018-05-31 23:58:59 +0800

    update run.sh

---
+++ node_exporter/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+go run get_export.go stress-gin 10 0.4
+go run get_export.go stress-gin 20 0.4
+go run get_export.go stress-gin 30 0.4
+go run get_export.go stress-gin 40 0.4
+go run get_export.go stress-gin 50 0.4
+go run get_export.go stress-gin 60 0.4
+
+go run get_export.go stress-gin 10 0.5
+go run get_export.go stress-gin 20 0.5
+go run get_export.go stress-gin 30 0.5
+go run get_export.go stress-gin 40 0.5
+go run get_export.go stress-gin 50 0.5
+go run get_export.go stress-gin 60 0.5
+
+go run get_export.go stress-gin 10 0.6
+go run get_export.go stress-gin 20 0.6
+go run get_export.go stress-gin 30 0.6
+go run get_export.go stress-gin 40 0.6
+go run get_export.go stress-gin 50 0.6
+go run get_export.go stress-gin 60 0.6
+
+go run get_export.go stress-gin 10 0.7
+go run get_export.go stress-gin 20 0.7
+go run get_export.go stress-gin 30 0.7
+go run get_export.go stress-gin 40 0.7
+go run get_export.go stress-gin 50 0.7
+go run get_export.go stress-gin 60 0.7
+
+go run get_export.go stress-gin 10 0.8
+go run get_export.go stress-gin 20 0.8
+go run get_export.go stress-gin 30 0.8
+go run get_export.go stress-gin 40 0.8
+go run get_export.go stress-gin 50 0.8
+go run get_export.go stress-gin 60 0.8
+
+go run get_export.go stress-gin 10 0.9
+go run get_export.go stress-gin 20 0.9
+go run get_export.go stress-gin 30 0.9
+go run get_export.go stress-gin 40 0.9
+go run get_export.go stress-gin 50 0.9
+go run get_export.go stress-gin 60 0.9
+
+go run get_export.go stress-gin 10 1
+go run get_export.go stress-gin 20 1
+go run get_export.go stress-gin 30 1
+go run get_export.go stress-gin 40 1
+go run get_export.go stress-gin 50 1
+go run get_export.go stress-gin 60 1
