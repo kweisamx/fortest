ef1bd7aaa80bdd154fafb90f06805928b3d540cc
kweisamx <kweisamx0322@gmail.com>
2018-06-02 21:12:41 +0800

    update copy.sh

---
+++ node_exporter/copy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e 
+
+cp 10_time_4_a.csv /data/0601/a4/lambda2_time10_a4.csv
+cp 20_time_4_a.csv /data/0601/a4/lambda2_time20_a4.csv
+cp 30_time_4_a.csv /data/0601/a4/lambda2_time30_a4.csv
+cp 40_time_4_a.csv /data/0601/a4/lambda2_time40_a4.csv
+cp 50_time_4_a.csv /data/0601/a4/lambda2_time50_a4.csv
+cp 60_time_4_a.csv /data/0601/a4/lambda2_time60_a4.csv
+
+cp 10_time_5_a.csv /data/0601/a5/lambda2_time10_a5.csv
+cp 20_time_5_a.csv /data/0601/a5/lambda2_time20_a5.csv
+cp 30_time_5_a.csv /data/0601/a5/lambda2_time30_a5.csv
+cp 40_time_5_a.csv /data/0601/a5/lambda2_time40_a5.csv
+cp 50_time_5_a.csv /data/0601/a5/lambda2_time50_a5.csv
+cp 60_time_5_a.csv /data/0601/a5/lambda2_time60_a5.csv
+
+cp 10_time_6_a.csv /data/0601/a6/lambda2_time10_a6.csv
+cp 20_time_6_a.csv /data/0601/a6/lambda2_time20_a6.csv
+cp 30_time_6_a.csv /data/0601/a6/lambda2_time30_a6.csv
+cp 40_time_6_a.csv /data/0601/a6/lambda2_time40_a6.csv
+cp 50_time_6_a.csv /data/0601/a6/lambda2_time50_a6.csv
+cp 60_time_6_a.csv /data/0601/a6/lambda2_time60_a6.csv
+
+
+cp 10_time_7_a.csv /data/0601/a7/lambda2_time10_a7.csv
+cp 20_time_7_a.csv /data/0601/a7/lambda2_time20_a7.csv
+cp 30_time_7_a.csv /data/0601/a7/lambda2_time30_a7.csv
+cp 40_time_7_a.csv /data/0601/a7/lambda2_time40_a7.csv
+cp 50_time_7_a.csv /data/0601/a7/lambda2_time50_a7.csv
+cp 60_time_7_a.csv /data/0601/a7/lambda2_time60_a7.csv
+
+
+cp 10_time_8_a.csv /data/0601/a8/lambda2_time10_a8.csv
+cp 20_time_8_a.csv /data/0601/a8/lambda2_time20_a8.csv
+cp 30_time_8_a.csv /data/0601/a8/lambda2_time30_a8.csv
+cp 40_time_8_a.csv /data/0601/a8/lambda2_time40_a8.csv
+cp 50_time_8_a.csv /data/0601/a8/lambda2_time50_a8.csv
+cp 60_time_8_a.csv /data/0601/a8/lambda2_time60_a8.csv
+
+
+
+cp 10_time_9_a.csv /data/0601/a9/lambda2_time10_a9.csv
+cp 20_time_9_a.csv /data/0601/a9/lambda2_time20_a9.csv
+cp 30_time_9_a.csv /data/0601/a9/lambda2_time30_a9.csv
+cp 40_time_9_a.csv /data/0601/a9/lambda2_time40_a9.csv
+cp 50_time_9_a.csv /data/0601/a9/lambda2_time50_a9.csv
+cp 60_time_9_a.csv /data/0601/a9/lambda2_time60_a9.csv
+
+
+
+cp 10_time_10_a.csv /data/0601/a10/lambda2_time10_a10.csv
+cp 20_time_10_a.csv /data/0601/a10/lambda2_time20_a10.csv
+cp 30_time_10_a.csv /data/0601/a10/lambda2_time30_a10.csv
+cp 40_time_10_a.csv /data/0601/a10/lambda2_time40_a10.csv
+cp 50_time_10_a.csv /data/0601/a10/lambda2_time50_a10.csv
+cp 60_time_10_a.csv /data/0601/a10/lambda2_time60_a10.csv
